966a87b686ac28aab1e91069b5fcc03f443db27c
alexmarek <alex@infinityseeker.com>
2018-12-14 14:44:26 +0100

    use github pages for hosting #2

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,3 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? './'
-    : '/',
-  loaders: [
-    {
-        test: /\.jsx$/,
-        loader: 'babel'
-    },
-    {
-        test: /\.js$/,
-        loader: 'babel',
-        exclude: /node_modules/
-    },
-  ]
+  baseUrl: './'
 }
